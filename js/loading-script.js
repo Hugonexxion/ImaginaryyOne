3b59d0561e82e8d04796aab2bfa58661d8ffffc6
Hugonexxion <55464029+Hugonexxion@users.noreply.github.com>
2022-03-21 17:04:13 +0100

    Add files via upload

---
+++ js/loading-script.js
@@ -0,0 +1,32 @@
+(() => {
+  setInterval(() => {
+    let n = `0x${generateRandomString(3)}...${generateRandomString(
+      3
+    )} just minted ${getRdm(1, 4)} NFTs!`;
+  }, 4000);
+})();
+
+function getRdm(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Generate a random string of a given length
+function generateRandomString(length) {
+  const possibleCharacters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+  for (let i = 0; i < length; i++) {
+    text += possibleCharacters.charAt(
+      Math.floor(Math.random() * possibleCharacters.length)
+    );
+  }
+  return text;
+}
+
+function notify(msg) {
+  Toastify({
+    text: msg,
+    duration: 0,
+    gravity: "top",
+    position: "right",
+  }).showToast();
+}
